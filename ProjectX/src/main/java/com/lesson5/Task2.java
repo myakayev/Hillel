23da8295ebc3ec3131400cdcf8e165bc3b73f476
myakayev <myakayev@yahoo.com>
2022-03-31 20:20:14 +0300

    Arithmetic operations

---
+++ ProjectX/src/main/java/com/lesson5/Task2.java
@@ -0,0 +1,14 @@
+package com.lesson5;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int n = 1;
+        for (int i = 0; i < 100; i++) {
+            n = n * (i+1);
+            if (n > 1000) {
+                System.out.println(n);
+                break;
+            }
+        }
+    }
+}
---
+++ ProjectX/src/main/java/com/lesson5/Task_1.java
@@ -0,0 +1,12 @@
+package com.lesson5;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i != 12 && i != 65 ) {
+                System.out.println(i+1);
+            }
+        }
+    }
+}
+
